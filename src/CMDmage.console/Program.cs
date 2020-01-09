31ed1b9871b91cfef041bbcbd5d250c7d72f4630
Anton From <anton.from@afconsult.com>
2020-01-09 15:55:37 +0100

    Changed publish task to build as single trimmed file for win-x64 architecture.

--- src/CMDmage.console/Program.cs
+++ src/CMDmage.console/Program.cs
@@ -7,6 +7,8 @@ namespace CMDmage.console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
